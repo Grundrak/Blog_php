a98d7cd09434a58cfe9ca693572c5e9e5d7cb143
Grundrak <electrovilla66@gmail.com>
2024-05-13 21:45:07 +0100

    V2

---
+++ backend/views/corps/navbar.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="output.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+
+<body>
+
+    <nav
+        class="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen border border-gray-100 bg-[#7469B6] py-4 px-8 shadow backdrop-blur-lg  ">
+        <div class="px-4">
+            <div class="flex items-center justify-between ">
+                <div class="flex shrink-0 md:gap-56">
+
+
+                    <div class="flex items-center justify-between gap-5 md:flex md:gap-40">
+                        <!-- Menu de Navigation -->
+                        <div class="flex gap-16 md:items-center md:justify-center">
+                            <a aria-current="page"
+                                class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-[#FFFFFF] transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                                href="#">Home</a>
+                            <a class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-[#FFFFFF] transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                                href="#">Blog</a>
+                            <a class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-[#FFFFFF] transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                                href="#">Post</a>
+                            <a class="inline-block rounded-lg px-2 py-1 text-sm font-medium text-[#FFFFFF] transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
+                                href="#">Contact Us</a>
+                        </div>
+
+
+                    </div>
+                </div>
+
+
+
+
+                <div class="flex items-center justify-end gap-3 md:gap-40">
+                    <!-- Barre de Recherche -->
+                    <div class="relative mx-auto text-gray-600">
+                        <input
+                            class="border border-gray-300 h-10 w-80 px-5 pr-16 rounded-2xl text-sm placeholder-current focus:outline-none dark:bg-gray-500 dark:border-gray-50 dark:text-[#7469B6]"
+                            type="search" name="search" placeholder="   Search...">
+
+                    </div>
+                    <div class="flex gap-7">
+                        <a class="hidden items-center  justify-center rounded-3xl bg-white px-4 py-2 text-sm font-semibold text-[#7469B6] shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150  sm:inline-flex"
+                            href="/login">Sign in</a>
+                        <a class="inline-flex items-center justify-center rounded-3xl bg-[#AD88C6] px-4 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                            href="/login">Register</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </nav>
+
+  
+
+
+
+
+
+
+</body>
+
+</html>

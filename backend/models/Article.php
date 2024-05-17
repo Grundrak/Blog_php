af54a8e0a6aae94fb309f3636ca81a8cb7ac8048
Grundrak <electrovilla66@gmail.com>
2024-05-17 10:38:26 +0100

    Fixe merge

--- backend/models/Article.php
+++ backend/models/Article.php
@@ -26,21 +26,15 @@ class Article {
             $params[] = $imagePath;
         }
 
+    
+        $sql .= " WHERE id = ?";
+        $params[] = $id;
 
-//         $sql .= " WHERE id = ?";
-//         $params[] = $id;
+        $stmt = $this->db->prepare($sql);
+        return $stmt->execute($params);
+    }
 
-//         $stmt = $this->db->prepare($sql);
-//         return $stmt->execute($params);
-//     }
-
-    public function getArticles() {
-        try {
-            $sql = "SELECT * FROM articles";
-            $stmt = $this->db->prepare($sql);
-            $stmt->execute();
-
-
+  
     public function getArticles() {
         $sql = "SELECT * FROM articles";
         $stmt = $this->db->prepare($sql);
